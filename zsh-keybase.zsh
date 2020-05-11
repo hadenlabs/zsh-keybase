2b1c6ecd538071659682b7214dea9f9a4743c3f1
@slovacus <slovacus@gmail.com>
2020-05-10 23:40:11 -0500

    chore: implement install keybase (#3)

---
+++ zsh-keybase.zsh
@@ -0,0 +1,38 @@
+#!/usr/bin/env ksh
+# -*- coding: utf-8 -*-
+
+#
+# Defines install keybase for osx or linux.
+#
+# Authors:
+#   Luis Mayta <slovacus@gmail.com>
+#
+export KEYBASE_PACKAGE_NAME="keybase"
+
+KEYBASE_PLUGIN_DIR="$(dirname "${0}")"
+KEYBASE_SOURCE_PATH="${KEYBASE_PLUGIN_DIR}"/src
+
+export KEYBASE_MESSAGE_BREW="Please install brew or use antibody bundle luismayta/zsh-brew branch:develop"
+export KEYBASE_MESSAGE_YAY="Please install Go or use antibody bundle luismayta/zsh-goenv branch:develop"
+export KEYBASE_MESSAGE_NOT_FOUND="this not found installed"
+
+# shellcheck source=/dev/null
+source "${KEYBASE_SOURCE_PATH}"/base.zsh
+
+# keybase::cross::os functions for osx and linux
+function keybase::cross::os {
+
+    case "${OSTYPE}" in
+        linux*)
+            # shellcheck source=/dev/null
+            source "${KEYBASE_SOURCE_PATH}"/linux.zsh
+            ;;
+        darwin*)
+            # shellcheck source=/dev/null
+            source "${KEYBASE_SOURCE_PATH}"/osx.zsh
+            ;;
+    esac
+
+}
+
+keybase::cross::os
